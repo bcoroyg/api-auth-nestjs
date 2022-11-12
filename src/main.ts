f34dea5df1f80fb6694ca02510b8495ae315bf36
Emilio Coroy <becgonzalez25@gmail.com>
2022-11-12 14:31:31 -0600

    variables de entorno

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PORT } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger();
+
+  //Variable de entorno PORT
+  const config = app.get(ConfigService);
+  const port = parseInt(config.get<string>(PORT), 10);
+
+  await app.listen(port);
+  logger.log(`Server in running in ${await app.getUrl()}`);
 }
 bootstrap();
