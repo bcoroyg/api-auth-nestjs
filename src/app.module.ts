3cd75e99e3accabe5ec1305469076ccf9082dd81
Emilio Coroy <becgonzalez25@gmail.com>
2022-11-12 14:37:21 -0600

    configuración de typeORM con mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,28 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from './config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>(DB_HOST),
+        port: parseInt(config.get<string>(DB_PORT), 10),
+        username: config.get<string>(DB_USER),
+        password: config.get<string>(DB_PASSWORD),
+        database: config.get<string>(DB_NAME),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
   ],
   controllers: [AppController],
