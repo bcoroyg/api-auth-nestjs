e27865536c27ed744582b08948edcfe2dbbb3ed9
Emilio Coroy <becgonzalez25@gmail.com>
2023-01-16 21:20:28 -0600

    Cambiar contraseña - parte 2

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Body, Controller, Get, Patch, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/users/decorators';
+import { UserEntity } from 'src/users/entities';
 import { AuthService } from './auth.service';
 import {
   ActivateUserDto,
+  ChangePasswordDto,
   LoginDto,
   RequestResetPasswordDto,
   ResetPasswordDto,
@@ -31,5 +43,15 @@ export class AuthController {
   @Patch('reset-password')
   resetPassword(@Body() resetPasswordDto: ResetPasswordDto): Promise<void> {
     return this.authService.resetPassword(resetPasswordDto);
+  }
+
+  @Patch('change-password')
+  @UseGuards(AuthGuard())
+  changePassword(
+    @Body() changePasswordDto: ChangePasswordDto,
+    @GetUser() user: UserEntity,
+  ): Promise<void> {
+    const a = 1;
+    return;
   }
 }
--- src/auth/dtos/index.ts
+++ src/auth/dtos/index.ts
@@ -2,3 +2,4 @@ export * from './login.dto';
 export * from './activate-user.dto';
 export * from './request-reset-password.dto';
 export * from './reset-password.dto';
+export * from './change-password.dto';
